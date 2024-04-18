1985759777771cd5ccc2f29a1d16e692fc3b2777
romaissaMe <95141338+romaissaMe@users.noreply.github.com>
2024-04-18 19:18:02 +0100

    Time: 43 ms (9.36%), Space: 16.7 MB (62.29%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -1,8 +1,8 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
         if not head or not head.next or left==right:
@@ -10,24 +10,23 @@ class Solution:
 
         dummy = ListNode(0)
         dummy.next = head
-        prev = dummy
+        prev=dummy
 
-        for _ in range(left - 1):
-            prev = prev.next
+        for i in range(left-1):
+            prev=prev.next
 
-        curr = prev.next
-        tail = curr
-        prev.next = None
+        curr=prev.next
+        tail=curr
+        prev.next= None
 
-        for _ in range(right - left + 1):
+        for i in range(right-left+1):
             next_node = curr.next
-            curr.next = prev.next
-            prev.next = curr
-            curr = next_node
-
-        # Connect the reversed sublist back to the original list
+            curr.next=prev.next
+            prev.next=curr
+            curr=next_node
+            
         tail.next = curr
-
         return dummy.next
 
-        
+
+       
