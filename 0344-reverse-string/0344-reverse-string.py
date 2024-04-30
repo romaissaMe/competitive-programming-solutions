73f275f2300c7d728bf01b61410deed0903a27c2
romaissaMe <95141338+romaissaMe@users.noreply.github.com>
2024-04-30 20:41:06 +0100

    Time: 168 ms (65.75%), Space: 22.8 MB (5.06%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,8 +1,13 @@
-class Solution(object):
-    def reverseString(self, s):
+class Solution:
+    
+    def reverse(self, s: List[str], left: int, right: int) -> None:
+        if left >= right:
+            return
+        s[left], s[right] = s[right], s[left]
+        self.reverse(s, left + 1, right - 1)
+
+    def reverseString(self, s: List[str]) -> None:
         """
-        :type s: List[str]
-        :rtype: None Do not return anything, modify s in-place instead.
+        Do not return anything, modify s in-place instead.
         """
-        return s.reverse()
-        
+        self.reverse(s, 0, len(s) - 1)
