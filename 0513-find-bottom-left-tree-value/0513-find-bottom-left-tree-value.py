87a33476d1f1c92dcb92d36d17ae2113bcbeb577
romaissaMe <95141338+romaissaMe@users.noreply.github.com>
2024-05-17 19:37:23 +0100

    Time: 47 ms (47.11%), Space: 18.3 MB (47.98%) - LeetHub

---
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+from collections import deque 
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        q = deque([root])
+        while q:
+            node = q.popleft()
+            if node.right:
+                q.append(node.right)
+            if node.left:
+                q.append(node.left)
+        
+        return node.val
+        
