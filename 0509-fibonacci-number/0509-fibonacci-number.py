336b03baf889c1b4a9fa07fb707f6d2a764fdeed
romaissaMe <95141338+romaissaMe@users.noreply.github.com>
2024-08-06 19:02:08 +0100

    Time: 29 ms (92.15%), Space: 16.4 MB (88.71%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def __init__(self):
+        self.memo = {}
+        
+    def fib(self, n: int) -> int:
+        if n in self.memo:
+            return self.memo[n]
+
+        if n==0:
+            return 0
+        
+        if n==1:
+            return 1
+        
+        self.memo[n] = self.fib(n-1) + self.fib(n-2)
+        return self.memo[n]
+        
